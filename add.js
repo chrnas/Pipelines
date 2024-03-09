301b144aa8b4013abc5c3f7f0bd6bb027b2e002a
Christoffer <chrna581@student.liu.se>
2024-03-09 08:49:06 +0100

    Added add.js

---
+++ add.js
@@ -0,0 +1,11 @@
+// add.js
+
+/**
+ * Adds two numbers.
+ * @param {number} a The first number to be added.
+ * @param {number} b The second number to be added.
+ * @returns {number} The sum of the two numbers.
+ */
+export function add (a, b) {
+  return a + b
+}
