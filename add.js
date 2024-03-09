71aae2ccd273022238af33d9b830d9f03e3f2843
Christoffer <chrna581@student.liu.se>
2024-03-09 08:59:24 +0100

    Added linting config with prettier

--- add.js
+++ add.js
@@ -6,7 +6,6 @@
  * @param {number} b The second number to be added.
  * @returns {number} The sum of the two numbers.
  */
-export function add (a, b) {
-    
-  return a + b
+export function add(a, b) {
+  return a + b;
 }
